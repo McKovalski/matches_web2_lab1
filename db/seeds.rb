221464d252a9e269508bc21ac140d4be9efb0709
ninokovacic <nkovai888@gmail.com>
2022-10-30 09:26:02 +0100

    Try fix for timestamp

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'date'
+
 Team.create([
     {
         name: "Barcelona"
@@ -23,54 +25,54 @@ Team.create([
 
 Match.create([
     {
-        time_of_play: "2022-02-06 11:30:00.000000000 +0000",
+        :time_of_play => DateTime.strptime("02/06/2022 13:00", "%m/%d/%Y %H:%M"),
         home_team_id: 1,
         away_team_id: 2,
         home_team_score: 4,
         away_team_score: 3
     },
     {
-        time_of_play: "2022-02-06 15:00:00.000000000 +0000",
+        time_of_play: DateTime.strptime("02/06/2022 15:00", "%m/%d/%Y %H:%M"),
         home_team_id: 1,
         away_team_id: 3,
         home_team_score: 1,
         away_team_score: 2
     },
     {
-        time_of_play: "2022-02-07 15:30:00.000000000 +0000",
+        time_of_play: DateTime.strptime("02/07/2022 15:00", "%m/%d/%Y %H:%M"),
         home_team_id: 1,
         away_team_id: 4,
         home_team_score: 2,
         away_team_score: 0
     },
     {
-        time_of_play: "2022-02-08 21:00:00.000000000 +0000",
+        time_of_play: DateTime.strptime("02/08/2022 18:00", "%m/%d/%Y %H:%M"),
         home_team_id: 2,
         away_team_id: 3,
         home_team_score: 4,
         away_team_score: 3
     },
     {
-        time_of_play: "2022-02-08 18:00:00.000000000 +0000",
+        time_of_play: DateTime.strptime("02/08/2022 21:00", "%m/%d/%Y %H:%M"),
         home_team_id: 2,
         away_team_id: 4,
         home_team_score: 1,
         away_team_score: 3
     },
     {
-        time_of_play: "2022-02-08 21:00:00.000000000 +0000",
+        time_of_play: DateTime.strptime("02/08/2022 21:00", "%m/%d/%Y %H:%M"),
         home_team_id: 3,
         away_team_id: 4,
         home_team_score: 0,
         away_team_score: 3
     },
     {
-        time_of_play: "2022-11-12 15:00:00.000000000 +0000",
+        time_of_play: DateTime.strptime("11/12/2022 18:00", "%m/%d/%Y %H:%M"),
         home_team_id: 3,
         away_team_id: 1
     },
     {
-        time_of_play: "2022-11-12 18:00:00.000000000 +0000",
+        time_of_play: DateTime.strptime("11/12/2022 21:00", "%m/%d/%Y %H:%M"),
         home_team_id: 4,
         away_team_id: 2
     }
